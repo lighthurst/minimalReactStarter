6108ea7923c040a3d4282c50b4f07524df42ea8a
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-08-02 13:46:28 -0700

    Move jest/enzyme setup file to proper folder.

--- client/spec/setupTestFiles.js
+++ client/spec/setupTestFiles.js
@@ -3,6 +3,6 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-global.shallow = shalow;
+global.shallow = shallow;
 global.render = render;
 global.mount = mount;
