192cb813ca610d8cf477a1550bb1c6de82c85be8
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-09-09 18:21:48 -0700

    Reformat and modularize server file, add error handling, and comment file.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,5 @@
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
+const app = require('./app');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-const PUBLIC_DIR = path.join(__dirname, '../public');
 
-app.use(express.json())
-  .use(express.static(PUBLIC_DIR))
-  .listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)); // eslint-disable-line no-console
